ec6041cc551a7860f6110cc23be8e028bcf484fb
Samantha Connolly <staylorconnolly@gmail.com>
2019-08-13 16:51:41 -0400

    startup

---
+++ Code/FlowerTimeAnalysis.py
@@ -0,0 +1,12 @@
+# This Python script can be run in any Integrated Development Environment. It uses the Seaborn library, which is not always
+# easy to get started with if you are not used to Python. If you have trouble getting the script to run, I recommend doing
+# the following: Install PyCharm, open the file in Pycharm, go to Preferences, Project, Project Interpreter, +, search for
+# Seaborn, and then click Install Package. I created a tutorial here: https://www.youtube.com/watch?v=vZWsVe2dBU8&t=1s
+# On my YouTube channel you can find more tutorials for Pycharm and Python.
+
+# first, let's import the Modules we will use
+import csv
+import Seaborn as sns
+
+# now, let's read in the file. change this filepath and filename as necessary.
+filepath = ""
--- README.md
+++
@@ -1,1 +0,0 @@
-# Blueberry Data Analysis for Erin
